f10e816320b4d634e6466887d3e00b13bd5c1cf6
Joaquim Magalhães <magalhaes1915@gmail.com>
2018-09-04 19:51:47 +0100

    Parse exams information

--- app/helpers/api/exams_helper.rb
+++ app/helpers/api/exams_helper.rb
@@ -1,2 +1,21 @@
 module Api::ExamsHelper
+  def self.parse(information)
+    return [] if information == []
+
+    data = []
+
+    information.each do |entry|
+      data << {
+          unit: entry['Disciplina'],
+          course: entry['Curso'],
+          type: entry['Tipologia'],
+          date: entry['Data'],
+          year: entry['AnoLectivo'],
+          room: entry['Sala'],
+          teachers: entry['Responsavel'].split('<br>')
+      }
+    end
+
+    data
+  end
 end
