a47d07386fb574f3c6e72c61e173b66e01f24b8a
Joaquim Magalhães <magalhaes1915@gmail.com>
2018-09-09 00:27:53 +0100

    Add smtp configurations to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.active_job.queue_adapter = :sidekiq
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address:              ENV['stmp_address'],
+      port:                 ENV['stmp_port'],
+      domain:               ENV['stmp_domain'],
+      user_name:            ENV['stmp_username'],
+      password:             ENV['stmp_password'],
+      authentication:       :login,
+      enable_starttls_auto: true
+  }
 end
