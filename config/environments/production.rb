ac062a678e87052e4d0db7520c800e1e45f26dab
Joaquim Magalhães <magalhaes1915@gmail.com>
2018-09-12 17:36:09 +0100

    Add middlewares to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.config.middleware.use APIMiddleware
+  Rails.application.config.middleware.use AdminMiddleware
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
