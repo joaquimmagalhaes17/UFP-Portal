0b1b59cf0eaa5963d534c5665a2e0d87aefe10e9
Joaquim Magalhães <magalhaes1915@gmail.com>
2018-09-16 12:19:15 +0100

    Add Cipher injection to rake task

--- lib/tasks/bot/analysis.rb
+++ lib/tasks/bot/analysis.rb
@@ -7,6 +7,7 @@ require "#{Rails.root}/app/models/partial_grade"
 require "#{Rails.root}/app/models/final_grade"
 require "#{Rails.root}/app/jobs/notify_partial_grade_job"
 require "#{Rails.root}/app/jobs/notify_final_grade_job"
+require "#{Rails.root}/app/helpers/cipher_helper"
 require 'digest'
 require 'json'
 
